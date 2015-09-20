6b85098e737f7374fc92356b55925dbdb1c1e755
Col McCaffrey <colleen31@gmail.com>
2015-09-20 18:06:21 -0400

    added working circle char title tip on hover, fade out

--- wif/public/scripts/d3.js
+++ wif/public/scripts/d3.js
@@ -37,31 +37,31 @@ var margin = {top: 10, right: 5, bottom: 8, left: 10},
 
     width = 1260 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
-var padding = 70;
+var padding = 10;
 var tip = d3.tip()
   .attr('class', 'd3-tip')
   .html(function(d) {
     return d.title + " " + d.year;
   })
-  .offset([-5, 0])
+  .offset([50, 0])
   //add css for tip - transition fade in and out
 d3.json("full_bechdel.json", function(data){
   var svg = d3.select("body")
  	.append("svg")
- 	.attr('id', 'viz')
+ 	//.attr('id', 'viz')
  	.attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
   .call(tip);
  
-  curses = svg.selectAll('circle')
+  movies = svg.selectAll('circle')
 		.data(data)
 		.enter()
 		.append('circle')
 		.attr('class', 'hvr-hang');
-		curses.attr('r', function(d, i) {
-          return Math.abs(d.rating) / d.rating * 10})
+		movies.attr('r', function(d, i) {
+          return Math.abs(d.rating) / d.rating * 5})
 		.attr('cx', function(d) {return Math.max(0 + padding, Math.random() * width - padding)})
 		.attr('cy', function(d) {return Math.max(0 + padding, Math.random() * height - padding)})
 	
@@ -83,7 +83,44 @@ d3.json("full_bechdel.json", function(data){
 					return "black"
 			}
 		})
-		.on('mouseover', tip.show)
-    .on('mouseout', tip.hide)
-})
+		.on('mousedown', tip.show)
+		.on('mouseover', animateFirstStep)
+		.on('mouseout', animateSecondStep)
+
+function animateFirstStep(){
+    //d3.select(this)
+    d3.select(this.parentNode.appendChild(this))
+
+      .transition()            
+        .delay(0)            
+        .duration(100)
+
+        .attr('r', function(d){
+        	if(d.rating === '3'){
+        		return 30;
+        	}
+        	else if(d.rating ==="2"){
+        		return 45;
+        	}
+        	else if(d.rating === "1"){
+        		return 50;
+        	}
+
+        });
+
+};
+
+function animateSecondStep(){
+    d3.select(this)
+      .transition()
+      .delay(500)
+      .duration(2000)
+      .each("end", tip.hide) //work on getting longer delay for tip.hide- find way to bring to front
+      //.style('pointer-events', 'none')
+
+      .attr('r', function(d, i) {
+          return Math.abs(d.rating) / d.rating * 5;
+        });
+		};
+});
 
